f97403e9b555ea8d822cf395cc89135515d3b08c
K.K. Lamberty <lamberty@morris.umn.edu>
2025-01-12 01:46:45 -0600

    Remove multiple import of the same path
    Deep Source brought this to my attention in the upstream repo once I cherry-picked a couple of commits in a PR there.

--- client/src/app/users/user-profile.component.spec.ts
+++ client/src/app/users/user-profile.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
-import { ActivatedRoute } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { throwError } from 'rxjs';
 import { ActivatedRouteStub } from '../../testing/activated-route-stub';
 import { MockUserService } from '../../testing/user.service.mock';
